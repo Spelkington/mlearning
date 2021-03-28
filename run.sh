fff5b37a9f1ee5885e9aef6ab748dd6f116568c4
Spencer Elkington <spelkington@gmail.com>
2021-03-27 19:25:38 -0600

    run script added

---
+++ run.sh
@@ -0,0 +1,11 @@
+python --version
+
+# Install necessary libraries
+pip install numpy
+pip install pandas
+
+# Install Jupyter Lab
+pip install jupyterlab
+
+# Run new JupyterLab session
+jupyter-lab
